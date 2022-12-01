0b264585c13d3e12c13a1bdc28604c940bed898d
Mariangelica Rodriguez <gromarant@gmail.com>
2022-12-01 21:43:17 +0100

    Make exercise of class 5, first week, module two

---
+++ 1-week-python/module-2-introduction-to-python/class-5-other-languages.py
@@ -0,0 +1,14 @@
+# Here's how printing "Hello, World" 10 times looks in Bash and Powershell:
+#     BASH:
+#         for i in {1..10}; do
+#             echo Hello, World!
+#     done
+
+#     POWERSHELL:
+#         for ($i=1; $i-le 10; $i++) {
+#             write-Host "Hello, World!"
+#         }
+
+# Now try out the Python example yourself:
+for i in range(10):
+    print("Hello, World!")
