9462d24420afc395b6a6be69c23da603a218ca58
Mariangelica Rodriguez <gromarant@gmail.com>
2022-12-01 21:44:04 +0100

    Make quiz, first week, module two

---
+++ 1-week-python/module-2-introduction-to-python/class-6-second-quiz.py
@@ -0,0 +1,24 @@
+# QUESTION 1  Fill in the correct Python command to put “My first Python program” onto the screen.
+print("My first Python program")
+
+# QUESTION 2  Python is an example of what type of programming language?
+    # Client-side scripting language
+    # Platform-specific scripting language
+    # General purpose scripting language  --------(correct answer)
+
+        # "You got it! Python is one of the general purpose scripting languages that are widely used for scripting and automation."
+
+    # Machine language 
+
+# QUESTION 3  Convert this Bash command into Python:
+print("Have a nice day")
+
+# QUESTION 4  Fill in the correct Python commands to put “This is fun!” onto the screen 5 times. 
+for i in range(5):
+    print("This is fun!")
+
+# QUESTION 5  Select the Python code snippet that corresponds to the following Javascript snippet:
+for i in range(10):
+    print(i)
+    
+        # "Right on! In Python, we use range() to initiate for loops."
