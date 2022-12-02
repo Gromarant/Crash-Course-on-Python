986255cb8ded7fdd756c14ca661f53b30b659db0
Mariangelica Rodriguez <gromarant@gmail.com>
2022-12-02 23:20:29 +0100

    Make exercise of class 3, first week, module three

---
+++ 1-week-python/module-3-hello-world/class-3-python-can-be-your-calculator.py
@@ -0,0 +1,5 @@
+# Now it’s your turn to try! Give this a go yourself.
+# Use Python to calculate (((1+2)*3)/4)^5(((1+2)∗3)/4)5
+ 
+#     Tip: remember that you can use a**b to calculate a to the power of b. 
+print((((1+2)*3)/4)**5)
