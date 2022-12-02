e513af03d580d8433f29944de0b627698082e402
Mariangelica Rodriguez <gromarant@gmail.com>
2022-12-02 23:19:56 +0100

    Make exercise of class 2, first week, module three

---
+++ 1-week-python/module-3-hello-world/class-2-getting-information.py
@@ -0,0 +1,4 @@
+# Give it a try yourself! In the following script, change the values of color and thing to have the computer output a different statement than the initial one.
+color = "aquamarine"
+thing = "Happyness"
+print(color + " is the color of " + thing)
